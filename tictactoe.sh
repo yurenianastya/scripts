514b3384199c904d36606a806a0167aaaf5e9298
yurenianastya <nastyayurenia@gmail.com>
2024-03-17 17:35:23 +0100

    adding computer option

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,6 +1,7 @@
 board=(" " " " " " " " " " " " " " " " " ")
 player=1
 input_msg="Invalid input. Please re-enter."
+computer=0
 
 win_combos=(
         "0 1 2" "3 4 5" "6 7 8"  # Rows
@@ -18,12 +19,14 @@ display_board() {
 
 save_game() {
     printf "%s\n" "${board[@]}" > saved_board.txt
-    echo "$player" > saved_player.txt
+    echo "$player" > mode.txt
+    echo "$computer" >> mode.txt
 }
 
 load_game() {
     readarray -t board < saved_board.txt
-    read player < saved_player.txt
+    player=$(sed -n '1p' mode.txt)
+    computer=$(sed -n '2p' mode.txt)
 }
 
 intro() {
@@ -37,7 +40,7 @@ intro() {
     display_board
     echo "To save the game, press S during your turn"
     echo "Would you like to start?"
-    echo "Press 1 for a new game, Press 2 to load saved game"
+    echo "Press 1 for a new game, Press 2 to load saved game, Press 3 for solo with Computer"
     while true; do
         read choice
     if [ "$choice" == "1" ]; then
@@ -47,8 +50,12 @@ intro() {
         load_game
         echo "Resume!"
         play
+    elif [ "$choice" == "3" ]; then
+        echo "Start with computer!"
+        computer=1
+        play
     else
-        echo input_msg
+        echo $input_msg
     fi
     done
 }
@@ -56,7 +63,10 @@ intro() {
 turn() {
     read -p "Enter your turn (0-8): " number
     if [[ "$number" =~ ^[0-8]$ && "${board[$number]}" == " " ]]; then
-        if (( player == 1 )); then
+        if [[ $computer == 1 ]]; then
+            player=1
+        fi
+        if [[ $player == 1 ]]; then
             board[$number]="X"
         else
             board[$number]="O"
@@ -74,14 +84,15 @@ turn() {
                 echo "Bye!"
                 exit 0
             else 
-               echo 
+               echo $input_msg
             fi
         done
     else
-        echo input_msg
+        echo $input_msg
         turn
     fi
 }
+
 
 check_winner() {
     for combo in "${win_combos[@]}"; do
@@ -101,11 +112,37 @@ check_winner() {
     exit 0
 }
 
+# Functions for computer play
+get_empty_places() {
+    empty_places=()
+    for ((i = 0; i < ${#board[@]}; i++)); do
+        if [[ "${board[$i]}" == " " ]]; then
+            empty_places+=("$i")
+        fi
+    done
+}
+
+computer_moves() {
+    get_empty_places
+    if [[ ${#empty_places[@]} -gt 0 ]]; then
+        random_index=$((RANDOM % ${#empty_places[@]}))
+        selected_move=${empty_places[$random_index]}
+        echo "Computer makes a move at position $selected_move"
+        board[$selected_move]="O"
+    else
+        echo "No empty places available on the board."
+    fi
+}
+
+
 play() {
     while true; do
         display_board
         turn
+        if [[ "$computer" == 1 ]]; then
+            computer_moves
         check_winner
+        fi
     done
 }
 
